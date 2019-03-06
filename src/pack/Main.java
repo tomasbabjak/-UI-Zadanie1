eefe3fbabe528a80bd7a6d6bd79c5c538d65cc9b
tomasbabjak <32101724+tomasbabjak@users.noreply.github.com>
2019-03-06 16:13:00 +0100

    Added methods getNumber, Warnsdorff

--- src/pack/Main.java
+++ src/pack/Main.java
@@ -26,6 +26,101 @@ public class Main {
         else return true;
     }
 
+    static int getNumber(int x, int y, int [][] board, int length){
+
+        int num = 0;
+
+        if(isValid(x-2,y+1, length) && isEmpty(board, x-2, y+1))
+            num++;
+        if(isValid(x-2,y-1, length) && isEmpty(board, x-2, y-1))
+            num++;
+        if(isValid(x-1,y+2, length) && isEmpty(board, x-1, y+2))
+            num++;
+        if(isValid(x-1,y-2, length) && isEmpty(board, x-1, y-2))
+            num++;
+        if(isValid(x+2,y+1, length) && isEmpty(board, x+2, y+1))
+            num++;
+        if(isValid(x+2,y-1, length) && isEmpty(board, x+2, y-1))
+            num++;
+        if(isValid(x+1,y+2, length) && isEmpty(board, x+1, y+2))
+            num++;
+        if(isValid(x+1,y-2, length) && isEmpty(board, x+1, y-2))
+            num++;
+
+        return num;
+    }
+
+    static boolean Warnsdorff(int nextX[], int [][] board, int length, int yy, int xx){
+
+        int min = N+1;
+        int n;
+
+        int x = xx;
+        int y = yy;
+
+        n = getNumber(x - 2, y + 1, board, length);
+        if (n < min) {
+            min = n;
+            nextX[0] = x - 2;
+            nextX[1] = y + 1;
+        }
+
+        n = getNumber(x - 2, y - 1, board, length);
+        if (n < min) {
+            min = n;
+            nextX[0] = x - 2;
+            nextX[1] = y - 1;
+        }
+
+        n = getNumber(x - 1, y + 2, board, length);
+        if (n < min) {
+            min = n;
+            nextX[0] = x - 1;
+            nextX[1] = y + 2;
+        }
+
+        n = getNumber(x - 1, y - 2, board, length);
+        if (n < min) {
+            min = n;
+            nextX[0] = x - 1;
+            nextX[1] = y - 2;
+        }
+
+        n = getNumber(x + 2, y + 1, board, length);
+        if (n < min) {
+            min = n;
+            nextX[0] = x + 2;
+            nextX[1] = y + 1;
+        }
+
+        n = getNumber(x + 2, y - 1, board, length);
+        if (n < min) {
+            min = n;
+            nextX[0] = x + 2;
+            nextX[1] = y - 1;
+        }
+
+        n = getNumber(x + 1, y + 2, board, length);
+        if (n < min) {
+            min = n;
+            nextX[0] = x + 1;
+            nextX[1] = y + 2;
+        }
+
+        n = getNumber(x + 1, y - 2, board, length);
+        if (n < min) {
+            min = n;
+            nextX[0] = x + 1;
+            nextX[1] = y - 2;
+        }
+
+        if(min != N+1)
+            return true;
+        else
+            return false;
+
+    }
+
     public static void main(String[] args) {
 	// write your code here
     }
